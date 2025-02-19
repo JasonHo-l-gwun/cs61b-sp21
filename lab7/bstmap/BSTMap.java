736820f44c9537d06d4e33fe7b9991e86ae6ebe6
Jason <3194739773@qq.com>
2025-02-20 00:39:23 +0800

    完成lab7

---
+++ lab7/bstmap/BSTMap.java
@@ -0,0 +1,192 @@
+package bstmap;
+
+import java.util.*;
+
+public class BSTMap <Key extends Comparable<Key>, Value> implements Map61B<Key,Value> {
+    private Node root;
+    private class Node {
+        public Key key;
+        public Value value;
+        public Node left;
+        public Node right;
+        int N;
+
+        Node(Key key, Value value) {
+            this.key = key;
+            this.value = value;
+            this.N = 1;
+        }
+    }
+
+    @Override
+    public void clear() {
+        while (root != null) {
+            Node t = root;
+            remove(root.key);
+        }
+    }
+
+    @Override
+    public boolean containsKey(Key key) {
+        return containsKey(root, key);
+    }
+
+    private boolean containsKey(Node x, Key key) {
+        if (x == null) return false;
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) return containsKey(x.left, key);
+        else if (cmp > 0) return containsKey(x.right, key);
+        else return true;
+    }
+
+    @Override
+    public Value get(Key key) {
+        Node t = get(root,key);
+        if (t == null) return null;
+        return t.value;
+    }
+
+    private Node get(Node x, Key key) {
+        if (x == null) return null;
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) return get(x.left, key);
+        else if (cmp > 0) return get(x.right, key);
+        else return x;
+    }
+
+    @Override
+    public int size() {
+        return size(root);
+    }
+
+    private int size(Node x) {
+        if (x == null) return 0;
+        else return x.N;
+    }
+
+    @Override
+    public void put(Key key, Value value) {
+        root = put(root, key, value);
+    }
+
+    private Node put(Node x, Key key, Value value) {
+        if (x == null) return new Node(key,value);
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) x.left = put(x.left, key, value);
+        else if (cmp > 0) x.right = put(x.right, key, value);
+        else x.value = value;
+        x.N = size(x.left) + size(x.right) + 1;
+        return x;
+    }
+
+    @Override
+    public Set<Key> keySet() {
+        Set<Key> keySet = new HashSet<>();
+        keySetHelper(root, keySet);
+        return keySet;
+    }
+    
+    private void keySetHelper(Node node, Set<Key> keySet) {
+        if (node == null) return;
+        keySetHelper(node.left, keySet);
+        keySet.add(node.key);
+        keySetHelper(node.right, keySet);
+    }
+
+    @Override
+    public Value remove(Key key) {
+        if (containsKey(key)) {
+            Value value = get(key);
+            root = remove(root, key);
+            return value;
+        }
+        return null;
+    }
+
+    private Node remove(Node x, Key key) {
+        if (x == null) return null;
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) x.left = remove(x.left, key);
+        else if (cmp > 0) x.right = remove(x.right, key);
+        else {
+            if (x.left == null) {
+                return x.right;
+            } else if (x.right == null) {
+                return x.left;
+            } else {
+                Node t = x;
+                x = min(x.right);
+                x.right = deleteMin(x.right);
+                x.left = t.left;
+            }
+        }
+        x.N = size(x.left) + size(x.right) + 1;
+        return x;
+    }
+
+    private Node min(Node x) {
+        if (x.left == null) return x;
+        else return min(x.left);
+    }
+
+    private Node max(Node x) {
+        if (x.right == null) return x;
+        else return max(x.right);
+    }
+
+    private Node deleteMin(Node x) {
+        if (x == null) return null;
+        x.left = deleteMin(x.left);
+        x.N = size(x.left) + size(x.right) + 1;
+        return x;
+    }
+
+    @Override
+    public Value remove(Key key, Value value) {
+        Value val = get(key);
+        if (val == value) remove(key);
+        return val;
+    }
+
+    @Override
+    public Iterator<Key> iterator() {
+        return new BSTIterator();
+    }
+
+    private void keys(Node x, Queue<Key> queue) {
+        if (x == null) return;
+        keys(x.left, queue);
+        queue.add(x.key);
+        keys(x.right, queue);
+    }
+
+    public void printInOrder() {
+        printInOrder(root);
+    }
+
+    private void printInOrder(Node x) {
+        if (x == null) return;
+        printInOrder(x.left);
+        System.out.println(x.key);
+        printInOrder(x.right);
+    }
+
+    private class BSTIterator implements Iterator{
+        private Queue<Key> queue;
+
+        BSTIterator() {
+            queue = new LinkedList<>();
+            keys(root, queue);
+        }
+
+        @Override
+        public boolean hasNext() {
+            return !queue.isEmpty();
+        }
+
+        @Override
+        public Key next() {
+            return queue.remove();
+        }
+    }
+}
