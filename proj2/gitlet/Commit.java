f40894fa9e985c4cc16ba3583ca996670d250ecb
Jason Ho <3194739773@qq.com>
2025-02-26 09:30:38 +0800

    Do a part of gitlet

--- proj2/gitlet/Commit.java
+++ proj2/gitlet/Commit.java
@@ -2,15 +2,19 @@ package gitlet;
 
 // TODO: any imports you need here
 
+import javax.xml.crypto.Data;
+import java.io.Serializable;
 import java.util.Date; // TODO: You'll likely use this in this class
+import java.util.Formatter;
+import java.util.TreeMap;
 
 /** Represents a gitlet commit object.
  *  TODO: It's a good idea to give a description here of what else this Class
  *  does at a high level.
  *
- *  @author TODO
+ *  @author Jason Ho
  */
-public class Commit {
+public class Commit implements Serializable {
     /**
      * TODO: add instance variables here.
      *
@@ -21,6 +25,34 @@ public class Commit {
 
     /** The message of this Commit. */
     private String message;
+    /** THe author of this Commit.*/
+    private String author;
+    /** THe timestamp of this Commit.*/
+    private Date date;
+    /** THe hash of this Commit.*/
+    private String uid;
+    /** THe hash of this Commit's parent 1.*/
+    private String parents1;
+    /** THe hash of this Commit's parent 2.*/
+    private String parents2;
+    /** THe blobs of this Commit.*/
+    private TreeMap<String,String> blobs;
+
 
     /* TODO: fill in the rest of this class. */
+    Commit(String message, Date date, String parents1, String parents2, TreeMap<String,String> blobs) {
+        this.date = date;
+        this.message = message;
+        this.blobs = blobs;
+        this.parents1 = parents1;
+        this.parents2 = parents2;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public TreeMap<String,String> getBlobs() {
+        return this.blobs;
+    }
 }
--- proj2/gitlet/Repository.java
+++ proj2/gitlet/Repository.java
@@ -1,6 +1,10 @@
 package gitlet;
 
 import java.io.File;
+import java.util.Date;
+import java.util.Formatter;
+import java.util.TreeMap;
+
 import static gitlet.Utils.*;
 
 // TODO: any imports you need here
@@ -9,7 +13,7 @@ import static gitlet.Utils.*;
  *  TODO: It's a good idea to give a description here of what else this Class
  *  does at a high level.
  *
- *  @author TODO
+ *  @author Jason Ho
  */
 public class Repository {
     /**
@@ -24,6 +28,71 @@ public class Repository {
     public static final File CWD = new File(System.getProperty("user.dir"));
     /** The .gitlet directory. */
     public static final File GITLET_DIR = join(CWD, ".gitlet");
-
     /* TODO: fill in the rest of this class. */
+    /** The commits directory */
+    public static final File commitsFile = join(GITLET_DIR, "commits");
+    /** The blobs directory */
+    public static final File blobsFile = join(GITLET_DIR, "blobs");
+    /** The addStaged directory */
+    public static final File addFile = join(GITLET_DIR, "add");
+    /** The rmStaged directory */
+    public static final File rmFile = join(GITLET_DIR, "rm");
+    /** The head pointer */
+    public static String head;
+    /** the branches */
+    public static TreeMap<String,String> branches;
+    public static String currentBranch;
+    /** staged for additions */
+    public static TreeMap<String,String> addStaged;
+    /** staged for removal */
+    public static TreeMap<String,String> rmStaged;
+
+
+    public static void init() {
+        if (!GITLET_DIR.exists()) {
+            GITLET_DIR.mkdir();
+            Commit initCommit = new Commit("initial commit", new Date(0), null, null, null);
+            byte[] serializedCommit = Utils.serialize(initCommit);
+            String commitHash = Utils.sha1(serializedCommit);
+            initCommit.setUid(commitHash);
+            File currentCommitFile = join(commitsFile, commitHash);
+            writeObject(currentCommitFile, initCommit);
+            branches = new TreeMap<>();
+            currentBranch = "master";
+            head = commitHash;
+            branches.put(currentBranch, commitHash);
+            blobsFile.mkdir();
+            addFile.mkdir();
+            rmFile.mkdir();
+        } else {
+            System.out.println("A Gitlet version-control system already exists in the current directory.");
+            System.exit(0);
+        }
+    }
+
+    public static void add(String fileName) {
+        File addFile = join(CWD, fileName);
+        if (!addFile.exists()) {
+            System.out.println("File does not exist.");
+            System.exit(0);
+        }
+
+        byte[] fileByte = readContents(addFile);
+        String fileHash = Utils.sha1(fileByte);
+        /** If the staged for addition has the file is same to the add file,don't add it */
+        boolean sameFileInStage = addStaged.containsKey(fileName) && addStaged.get(fileName).equals(fileHash);
+        if (sameFileInStage) return;
+        File currentCommitFile = join(commitsFile, head);
+        Commit currentCommit = Utils.readObject(currentCommitFile, Commit.class);
+        boolean sameFileInCommit = currentCommit.getBlobs().containsKey(fileName) && currentCommit.getBlobs().get(fileName).equals(fileHash);
+        if (sameFileInCommit && addStaged.containsKey(fileName)) {
+            addStaged.remove(fileName);
+            Utils.writeObject(addFile, addStaged);
+            return;
+        } else if(sameFileInCommit) return;
+        rmStaged.remove(fileName);
+        Utils.writeObject(addFile, rmStaged);
+        addStaged.put(fileName, fileHash);
+        Utils.writeObject(addFile, addStaged);
+    }
 }
