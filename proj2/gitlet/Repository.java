028a9e2770299be1a8909898107da53990917117
Jason Ho <3194739773@qq.com>
2025-03-02 14:35:27 +0800

    Improve the style

--- proj2/gitlet/Repository.java
+++ proj2/gitlet/Repository.java
@@ -6,14 +6,12 @@ import java.util.*;
 import static gitlet.Utils.*;
 
 /** Represents a gitlet repository.
- *  TODO: It's a good idea to give a description here of what else this Class
  *  does at a high level.
  *
  *  @author Jason Ho
  */
 public class Repository {
     /**
-     * TODO: add instance variables here.
      *
      * List all instance variables of the Repository class here with a useful
      * comment above them describing what that variable represents and how that
@@ -24,7 +22,6 @@ public class Repository {
     public static final File CWD = new File(System.getProperty("user.dir"));
     /** The .gitlet directory. */
     public static final File GITLET_DIR = join(CWD, ".gitlet");
-    /* TODO: fill in the rest of this class. */
     /** The commits directory */
     public static final File COMMIT_DIR = join(GITLET_DIR, "commits");
     /** The blobs directory */
@@ -44,8 +41,8 @@ public class Repository {
 
     public static void init() {
         if (GITLET_DIR.exists()) {
-            System.out.println("A Gitlet version-control system " +
-                    "already exists in the current directory.");
+            System.out.println("A Gitlet version-control system "
+                    + "already exists in the current directory.");
             System.exit(0);
         }
         GITLET_DIR.mkdir();
@@ -689,8 +686,8 @@ public class Repository {
         TreeMap<String, String> targetBlobs = targetCommit.getBlobs();
         for (String fileName : untrackedFiles) {
             if (targetBlobs.containsKey(fileName)) {
-                System.out.println("There is an untracked file in the way;" +
-                        " delete it, or add and commit it first.");
+                System.out.println("There is an untracked file in the way;"
+                        + " delete it, or add and commit it first.");
                 System.exit(0);
             }
         }
